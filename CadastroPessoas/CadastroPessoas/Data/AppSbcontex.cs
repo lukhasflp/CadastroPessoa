87445645a56b647f6c06246ebc4145c097ce4763
Lucas Felipe <133154877+lukhasflp@users.noreply.github.com>
2024-07-31 14:12:21 -0300

    Add files via upload

---
+++ CadastroPessoas/CadastroPessoas/Data/AppSbcontex.cs
@@ -0,0 +1,15 @@
+﻿using CadastroPessoas.models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CadastroPessoas.Data;
+
+    public class AppDbcontext : DbContext
+    {
+        public AppDbcontext(DbContextOptions options):base(options)
+    {
+
+    }
+        DbSet<Pessoa> Pessoas { get; set; } = null!;
+    }
+
---
+++ CadastroPessoas/CadastroPessoas/models/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿namespace CadastroPessoas.models
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; } = string.Empty;
+
+        public string CPF { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+    }
+}
